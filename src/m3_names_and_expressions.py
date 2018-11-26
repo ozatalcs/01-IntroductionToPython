17266f7189cd3ddb8cdb1c153fca1ddea5ae4543
ozatalcs <ozatalcs@rose-hulman.edu>
2018-11-26 12:34:51 -0500

    Did it

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -40,7 +40,7 @@ print(math.sqrt(Q))
 ###############################################################################
 print(math.sqrt(41 * 88 + 4*math.cos(2)))
 ###############################################################################
-# TODO: 4.
+# Done: 4.
 #   Immediately below this _TODO_,
 #   write code that computes the square root of 2 in two ways:
 #     - By using the   math.sqrt   function.
@@ -52,7 +52,7 @@ print(math.sqrt(41 * 88 + 4*math.cos(2)))
 print(math.sqrt(2))
 print(2**0.5)
 ###############################################################################
-# TODO: 5.
+# Done: 5.
 #   Every object has a TYPE and a VALUE.  For example,
 #   for the object that is computed by  math.sqrt(2):
 #      Its TYPE is float  (which is shorthand for "floating point number").
@@ -91,10 +91,22 @@ print(2**0.5)
 # After you have written and run the code to learn the TYPE
 # of each of the above, change the above _TODO_ to DONE.
 ###############################################################################
-
+print(type(3.14))
+print(type("hello"))
+print(type('hello'))
+print(type('a b c'))
+print(type(3 + 3))
+print(type("3" + "3"))
+print(type(2**100))
+print(type(2.0**100))
+print(type(math.sin(8)))
+print(type(math.sin))
+print(type(print))
+print(type(math))
+print(type('math'))
 ###############################################################################
 #
-# TODO: 6.
+# Done: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
